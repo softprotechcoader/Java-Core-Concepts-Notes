b7f0a291dab6222c9f7e17da5849b5900dd3ec74
Ayush <softprotechcoader@outlook.com>
2024-07-16 20:38:12 +0530

    Play with String

--- Getting_Started_Java/src/String_reverse.java
+++ Getting_Started_Java/src/String_reverse.java
@@ -19,3 +19,16 @@ public class String_reverse {
         System.out.println(revword);
     }
 }
+/*
+Time Complexity:The time complexity of the given code is O(n),
+where n is the length of the input string. This is because the
+for loop iterates through each character in the string once to
+create the reverse string.
+
+Space Complexity: The space complexity of the code is O(n),
+where n is the length of the input string. This is because
+the reverse string is created and stored in a separate string
+variable, which takes up space in memory proportional to the
+length of the input string. In addition, the other variables
+used in the code (i, str, and ans) take up a constant amount
+of space that is independent of the input size.*/
