66b7e8220d0e4e59cf14087c43e8f4c8e0361a5f
Ayush Vaibhav <softprotechcoder@outlook.com>
2025-09-08 01:18:12 +0530

    multilevel and multiple interface

--- Exploring Java/Oops/interfaceIntro.java
+++ Exploring Java/Oops/interfaceIntro.java
@@ -4,8 +4,15 @@
     void show();
     void config();
 } 
+ interface  iI2{
+        void run();
+        
+    }
+ interface ii3 extends iI2 {
 
-     class interfaceImpl implements iI {
+    
+}
+     class interfaceImpl implements iI,ii3 {
 
         @Override
         public void show() {
@@ -20,9 +27,17 @@
             // throw new UnsupportedOperationException("Unimplemented method 'config'");
             System.out.println("Implemented Config");
         }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            // throw new UnsupportedOperationException("Unimplemented method 'run'");
+            System.out.println("implemented run.....");
+        }
     
         
     }
+    
 
 public class interfaceIntro {
     public static void main(String[] args) {
@@ -31,5 +46,6 @@ public class interfaceIntro {
         impl.config();
         impl.show();
         System.out.println(interfaceImpl.num + "\n"+ interfaceImpl.place);
+        impl.run();
     }
 }
