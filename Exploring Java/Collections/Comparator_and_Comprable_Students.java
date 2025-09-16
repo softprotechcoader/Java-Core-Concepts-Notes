0432f92eddeaeeff55252909584e3ba3c6621c43
Ayush Vaibhav <softprotechcoder@outlook.com>
2025-09-16 07:41:01 +0530

    working on comparator and comparable

---
+++ Exploring Java/Collections/Comparator_and_Comprable_Students.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Comparator_and_Comprable_Students {
+     public static void main(String[] args) {
+
+        Comparator<Integer> com = new Comparator<Integer>(){
+        public int compare(Integer i, Integer j){
+            if(i%10 > j%10)
+            return 1;
+            else
+            return -1;
+        }
+    };
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(77);
+        nums.add(90);
+        nums.add(25);
+        nums.add(43);
+        // Collections.sort(nums);   //Collections is the class
+        Collections.sort(nums , com);
+        System.out.println(nums);
+
+    }
+}
