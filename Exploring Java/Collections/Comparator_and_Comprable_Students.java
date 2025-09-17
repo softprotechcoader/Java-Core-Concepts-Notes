dfd7658b44ddd6be03fdd3649d3422cba3c9f91f
Ayush Vaibhav <softprotechcoder@outlook.com>
2025-09-17 08:13:34 +0530

    working on comparator and comparable.

--- Exploring Java/Collections/Comparator_and_Comprable_Students.java
+++ Exploring Java/Collections/Comparator_and_Comprable_Students.java
@@ -3,26 +3,40 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+class Student{
+    int age;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+   
+    public String toString() {
+        return "Students [age=" + age + ", name=" + name + "]";
+    }
+    String name;
+
+}
+
 public class Comparator_and_Comprable_Students {
      public static void main(String[] args) {
 
-        Comparator<Integer> com = new Comparator<Integer>(){
-        public int compare(Integer i, Integer j){
-            if(i%10 > j%10)
+        Comparator<Student> com = new Comparator<Student>(){
+        public int compare(Student i, Student j){
+            if(i.age %10 > j.age %10)
             return 1;
             else
             return -1;
         }
     };
 
-        List<Integer> nums = new ArrayList<>();
-        nums.add(77);
-        nums.add(90);
-        nums.add(25);
-        nums.add(43);
-        // Collections.sort(nums);   //Collections is the class
-        Collections.sort(nums , com);
-        System.out.println(nums);
+        List<Student> student = new ArrayList<>();
+        student.add(new Student(25, "Ayush"));
+        student.add(new Student(24, "Amit"));
+        student.add(new Student(23, "Aditya"));
+        student.add(new Student(23, "Hrishikesh"));
+        // Collections.sort(students);   //Collections is the class
+        Collections.sort(student , com);
+        System.out.println(student);
 
     }
 }
