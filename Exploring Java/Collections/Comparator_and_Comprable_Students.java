55f3d7e5a8f0770593c64ac4e0f3c839e1b277f0
Ayush Vaibhav <softprotechcoder@outlook.com>
2025-09-18 07:30:09 +0530

    Implemented Comaprator logic using lemda expression as it is functional interface.

--- Exploring Java/Collections/Comparator_and_Comprable_Students.java
+++ Exploring Java/Collections/Comparator_and_Comprable_Students.java
@@ -6,7 +6,7 @@ import java.util.List;
 class Student implements Comparable<Student>{   // implemented comparable to achieve natural sorting. 
     int age;
     String name;
-    
+
     public Student(int age, String name) {
         this.age = age;
         this.name = name;
@@ -19,7 +19,7 @@ class Student implements Comparable<Student>{   // implemented comparable to ach
 
     @Override
     public int compareTo(Student that) {
-         if(this.age %10 > that.age %10)
+         if(this.age  > that.age )
             return 1;
             else
             return -1;
@@ -30,14 +30,9 @@ class Student implements Comparable<Student>{   // implemented comparable to ach
 public class Comparator_and_Comprable_Students {
      public static void main(String[] args) {
 
-    //     Comparator<Student> com = new Comparator<Student>(){
-    //     public int compare(Student i, Student j){
-    //         if(i.age %10 > j.age %10)
-    //         return 1;
-    //         else
-    //         return -1;
-    //     }
-    // };
+        Comparator<Student> com = (i,j)-> i.age > j.age ? 1 : -1;
+        
+    
 
         List<Student> student = new ArrayList<>();
         student.add(new Student(25, "Ayush"));
@@ -45,9 +40,9 @@ public class Comparator_and_Comprable_Students {
         student.add(new Student(23, "Aditya"));
         student.add(new Student(23, "Hrishikesh"));
 
-        Collections.sort(student);   //Collections is the class
+        // Collections.sort(student);   //Collections is the class
         // now, we can simply pass student in Collection.short.
-        // Collections.sort(student , com);
+        Collections.sort(student , com);
 
         System.out.println(student);
 
