4da6dd85b03ab92eacf58ddb6282e90625c06c37
Ayush Vaibhav <softprotechcoder@outlook.com>
2026-08-15 21:29:51 +0530

    Explored issues with non generics

--- Exploring Java/Generics/Main.java
+++ Exploring Java/Generics/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-       
+       Box box = new Box(10);
+         System.out.println(box.getValue());
+        //  Box box2 = new Box();
+        //  box2.setValue("Integer");
+        /*
+            So here we have a problem because we are trying to set a String value to a 
+            Box object that is designed to hold an Integer value. This will result in a
+            compile-time error, as the Box class is not designed to handle different data types. 
+            To fix this issue, we can use generics to create a Box class that can hold any type of value. 
+            Here's an example of how we can modify the Box class to use generics:  
+        */
     }
 }
